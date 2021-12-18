34167303257d9c0d44ff8eee1be02d80a0f5610e
Ketul8621 <77052683+Ketul8621@users.noreply.github.com>
2021-12-18 21:14:59 +0530

    Getting data manually

--- src/components/News.js
+++ src/components/News.js
@@ -3,25 +3,36 @@ import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
+import newsData from "../data";
 
 const News = (props) => {
+  //  console.log("data = ", newsData);
   const [articles, setArticles] = useState([]);
+  const [articlesPerPage, setArticlesPerPage] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(props.pageSize);
   const [totalArticles, setTotalArticles] = useState(0);
   const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  const update = async () => {
+  let parsedData = newsData[props.category];
+
+  const update = () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-    let data = await fetch(url);
+    //const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    //let data = await fetch(url);
     props.setProgress(40);
-    let parsedData = await data.json();
-    console.log(parsedData);
+    //let parsedData = await data.json();
+    console.log(props.category);
+    //let parsedData = newsData[props.category];
+    //console.log(parsedData);
     props.setProgress(70);
     setArticles(parsedData.articles);
+    setArticlesPerPage(parsedData.articles.slice(0, 6));
+    //console.log("slices = ", articlesPerPage);
+    //console.log(articles);
+    //console.log(parsedData.articles.slice(0, 6));
     setTotalArticles(parsedData.totalResults);
     setLoading(false);
     props.setProgress(100);
@@ -30,22 +41,32 @@ const News = (props) => {
   useEffect(() => {
     document.title = `${capitalize(props.category)} - Newszone`;
     update();
+    // eslint-disable-next-line
   }, []);
 
-  const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${
-      props.country
-    }&category=${props.category}&apiKey=${props.apiKey}&page=${
-      page + 1
-    }&pageSize=${props.pageSize}`;
-    setPage(page + 1);
-    let data = await fetch(url);
-    let parsedData = await data.json();
+  const fetchMoreData = () => {
+    //const url = `https://newsapi.org/v2/top-headlines?country=${
+    //  props.country
+    //}&category=${props.category}&apiKey=${props.apiKey}&page=${
+    //  page + 1
+    //}&pageSize=${props.pageSize}`;
+    //setPage(page + props.pageSize);
+    //let data = await fetch(url);
+    //let parsedData = await newsData.json();
     //setPage(page + 1);
-    console.log(parsedData);
-    setArticles(articles.concat(parsedData.articles));
+    //console.log(parsedData);
+    //setArticles(articles.concat(parsedData.articles));
+    //setInterval(() => {
+    setArticlesPerPage(
+      articlesPerPage.concat(
+        parsedData.articles.slice(page, page + props.pageSize)
+      )
+    );
+    console.log(parsedData.articles.slice(page, page + props.pageSize));
     setTotalArticles(parsedData.totalResults);
+    setPage(Math.min(page + props.pageSize, parsedData.totalResults));
     setLoading(false);
+    //}, 200);
   };
 
   return (
@@ -56,40 +77,40 @@ const News = (props) => {
       >
         Newszone - Top {capitalize(props.category)} Headlines
       </h1>
-      {loading && <Spinner />}
-      {console.log(articles.length)}
-      {/*{console.log(totalArticles)}*/}
+      {/*{loading && <Spinner />}*/}
+
       <InfiniteScroll
-        dataLength={articles.length}
+        dataLength={page}
         next={fetchMoreData}
-        hasMore={articles.length !== totalArticles}
+        hasMore={page !== totalArticles}
         loader={<Spinner />}
       >
         <div className="container">
           <div className="row">
-            {articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title.slice(0, 45) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : ""
-                    }
-                    imageUrl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/rockcms/2021-11/211126-pak-ind-cricket-mb-1439-9a0b46.jpg"
-                    }
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
+            {articles &&
+              articlesPerPage.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title.slice(0, 45) : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88)
+                          : ""
+                      }
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/rockcms/2021-11/211126-pak-ind-cricket-mb-1439-9a0b46.jpg"
+                      }
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
           </div>
         </div>
       </InfiniteScroll>
